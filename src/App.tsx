abb8b61805aa8c21a21f5f0a3b7a8c8d37f3d4e9
Paulo Barone <paulobaronedev@gmail.com>
2026-02-06 13:00:40 -0300

    finish project

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,131 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface FormDataType {
+  width: string;
+  height: string;
+  pixelPitch: string;
 }
 
-export default App
+function App() {
+  const [formData, setFormData] = useState<FormDataType>({
+    width: "",
+    height: "",
+    pixelPitch: "",
+  });
+  const [result, setResult] = useState<number | null>(null);
+
+  const getInchesFromResolution = () => {
+    const width = Number(formData.width);
+    const height = Number(formData.height);
+    const pixelPitch = Number(formData.pixelPitch);
+
+    if (
+      typeof width !== "number" ||
+      typeof height !== "number" ||
+      typeof pixelPitch !== "number" ||
+      isNaN(width) ||
+      isNaN(height) ||
+      isNaN(pixelPitch)
+    ) {
+      alert(
+        "Por favor, preencha todos os campos com valores numéricos válidos.",
+      );
+      setFormData({ width: "", height: "", pixelPitch: "" });
+      return;
+    }
+
+    const widthMM = width * pixelPitch;
+    const heightMM = height * pixelPitch;
+    console.log("Width in mm:", widthMM);
+    console.log("Height in mm:", heightMM);
+
+    const diagonalMM = Math.sqrt(Math.pow(widthMM, 2) + Math.pow(heightMM, 2));
+    console.log("Diagonal in mm:", diagonalMM);
+
+    const inches = diagonalMM / 25.4;
+    setResult(inches);
+  };
+
+  return (
+    <div className="container">
+      <h1 className="title">Calculadora de polegadas</h1>
+      <form
+        className="calc-form"
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <div className="resolution-inputs-container">
+          <label className="label" htmlFor="width">
+            Resolução em Pixels
+          </label>
+          <div className="resolution-inputs">
+            <input
+              className="input"
+              autoComplete="off"
+              id="width"
+              placeholder="Horizontal"
+              type="text"
+              pattern="^\d+$"
+              title="Digite apenas números"
+              value={formData.width}
+              onChange={(e) => {
+                setFormData((prev) => ({
+                  ...prev,
+                  width: e.target.value.replace(/\D/g, ""),
+                }));
+              }}
+            />
+            <input
+              className="input"
+              autoComplete="off"
+              placeholder="Vertical"
+              type="text"
+              pattern="^\d+$"
+              title="Digite apenas números"
+              value={formData.height}
+              onChange={(e) => {
+                setFormData((prev) => ({
+                  ...prev,
+                  height: e.target.value.replace(/\D/g, ""),
+                }));
+              }}
+            />
+          </div>
+        </div>
+        <div className="resolution-inputs">
+          <label className="label" htmlFor="pixel-pitch">
+            Pixel Pitch
+          </label>
+          <input
+            className="input"
+            autoComplete="off"
+            placeholder="Ex: 3.5"
+            id="pixel-pitch"
+            type="text"
+            pattern="^\d+(\.\d+)?$"
+            title="Digite um número válido (ex: 3.5, 3, 1.25)"
+            value={formData.pixelPitch}
+            onChange={(e) => {
+              setFormData((prev) => ({
+                ...prev,
+                pixelPitch: e.target.value.replace(/[^\d.]/g, ""),
+              }));
+            }}
+          />
+        </div>
+        <button className="calc-button" onClick={getInchesFromResolution}>
+          Calcular
+        </button>
+      </form>
+      {result && (
+        <div className="result-inch">
+          <h2>Resultado em Polegadas: {result.toFixed(2)}</h2>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
